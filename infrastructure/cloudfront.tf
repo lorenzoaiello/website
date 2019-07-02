891c32f60e9780f710714e8d237fa20f067d4792
Lorenzo Aiello <lorenzo@aiello.family>
2019-07-02 06:42:57 -0700

    fixing sni in cf

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -43,6 +43,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
   }
 }
 
