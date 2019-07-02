02c600373fc9c3d63fc245ed7dd404cb62ee1e0f
Lorenzo Aiello <lorenzo@aiello.family>
2019-07-02 06:28:26 -0700

    fixing syntax issue for r53

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -29,7 +29,7 @@ resource "aws_route53_record" "autodiscover" {
 resource "aws_route53_record" "cert_validation" {
   name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id = data.aws_route53_zone.primary.id
+  zone_id = aws_route53_zone.primary.id
   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
