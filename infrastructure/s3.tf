41e7b839c0ed4563ac1e60d289a2ccab6c5a7845
Lorenzo Aiello <lorenzo@6c61.com>
2019-07-11 18:18:47 -0700

    updating s3 bucket

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "website" {
-  bucket = "lorenzoaiello-entrypoint"
+  bucket = "lorenzo-aiello-family"
   acl    = "private"
   policy = data.aws_iam_policy_document.s3_policy.json
 
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "website" {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::lorenzoaiello-entrypoint/*"]
+    resources = ["arn:aws:s3:::lorenzo-aiello-family/*"]
 
     principals {
       type        = "AWS"
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "s3_policy" {
 
   statement {
     actions   = ["s3:ListBucket"]
-    resources = ["arn:aws:s3:::lorenzoaiello-entrypoint"]
+    resources = ["arn:aws:s3:::lorenzo-aiello-family"]
 
     principals {
       type        = "AWS"
